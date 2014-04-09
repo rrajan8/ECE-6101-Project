f031fef086f23c4dc033d8c5d7e809d9fb6462cb
jtshinsato <jtshinsato@gmail.com>
2014-04-09 11:48:09 -0400

    Added heartbeat to Worker

--- src/worker/Worker.java
+++ src/worker/Worker.java
@@ -3,6 +3,12 @@ package worker;
 import java.io.*;
 import java.net.*;
 
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import java.lang.*;
+
 import common.*;
 
 public class Worker {
@@ -11,6 +17,7 @@ public class Worker {
   int schedulerPort;
   String workerAddr;
   int workerPort;
+  int heartbeatPort;
 
   public static void main(String[] args) {
     Worker worker = new Worker( args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
@@ -22,6 +29,7 @@ public class Worker {
     schedulerPort = sPort;
     workerAddr = "localhost";
     workerPort = wPort;
+    heartbeatPort = wPort+100;
   }
 
   public void run() {
@@ -55,6 +63,11 @@ public class Worker {
       dos.close();
       socket.close();
 
+      //Create a heartbeat for the worker
+      TimerTask heartbeat = new WorkerHeartbeat(workerId, 4999); //4999 is the heartbeat scheduler's port might pass that through the stream
+      Timer timer = new Timer(true);
+      timer.scheduleAtFixedRate(heartbeat, 0, 10 * 1000); // 10 sec * 1000 ms
+
       System.out.println("This is worker "+workerId);
       
       //repeatedly process scheduler's task assignment
@@ -73,6 +86,8 @@ public class Worker {
         int taskId = dis.readInt();
         dis.close();
         socket.close();
+
+
         //read the job file from shared file system
         Job job = JobFactory.getJob(fileName, className);
         job.task(taskId);
@@ -100,6 +115,37 @@ public class Worker {
     } catch(Exception e) {
       e.printStackTrace();
     }
+  }
+
+  class WorkerHeartbeat extends TimerTask{
+    
+    private int workerID;
+    private int workerPort;
+    private int managerPort; //port that we'll send the heartbeat to
+
+    public WorkerHeartbeat(int worker_id , int manager_port){
+      System.out.println("Creating WorkerWatcher Thread");
+      workerID = worker_id;
+      workerPort = 4000+workerID; // Should probably find a cleaner way to do this later
+      managerPort = manager_port;
+    }
+
+      @Override
+      public void run(){
+        try{
+          //System.out.println("Sending heartbeat at:" + new Date());
+          Socket workerSocket = new Socket(workerAddr, managerPort); // should actually be the host of the server, but since it's all localhost, it doesn't matter
+          DataOutputStream wos = new DataOutputStream(workerSocket.getOutputStream());
+          System.out.println("Worker"+ workerID + " is sending a heartbeat at "+new Date());
+          wos.writeInt(workerID); // send worker ID 
+          wos.close();
+          //System.out.println("Done at:" + new Date());
+
+      }catch(Exception e) {
+          e.printStackTrace();
+        }
+      }
+
   }
 
 }
